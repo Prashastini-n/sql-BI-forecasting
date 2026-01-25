63411b1fa4bc7100b41beb2e284f0254fdf9eea8
Prashastini-n <prashastinin@gmail.com>
2026-01-25 19:53:44 +0530

    Create table

---
+++ sql/raw_table_load.sql
@@ -0,0 +1,11 @@
+CREATE TABLE orders_raw (
+    order_id VARCHAR(30),
+    order_date VARCHAR(20),
+    ship_date VARCHAR(20),
+    customer_id VARCHAR(20),
+    product_id VARCHAR(20),
+    region VARCHAR(20),
+    sales VARCHAR(20),
+    profit VARCHAR(20),
+    quantity VARCHAR(20)
+);
